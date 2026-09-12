a2e9bc5971f49d74773902c4e9ff92fd38db6ec9
nl7912458-cmd <nl7912458@gmail.com>
2026-09-12 21:04:24 +0700

    Update list-data.js

--- data/list-data.js
+++ data/list-data.js
@@ -4,13 +4,13 @@ export const listTaiLieu = [
         id: 'tailieu1',
         title: 'Chuyên đề: Con lắc lò xo & Lực đàn hồi',
         description: 'Hệ thống công thức độc lập với thời gian và kỹ thuật tối ưu hóa.',
-        date: '12/09/2026'
+        
     },
     {
         id: 'tailieu2',
         title: 'Ứng dụng vòng tròn lượng giác',
         description: 'Kỹ thuật chiếu vectơ trên sơ đồ hình tròn trong dao động.',
-        date: '15/09/2026'
+        
     }
 ];
 
