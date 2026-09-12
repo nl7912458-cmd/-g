1e30292c9b3f698904e916e8e71a40ba9e8094f1
nl7912458-cmd <nl7912458@gmail.com>
2026-09-12 22:09:37 +0700

    Update list-data.js

--- data/list-data.js
+++ data/list-data.js
@@ -3,14 +3,14 @@ export const listTaiLieu = [
     {
         id: 'tailieu1',
         title: 'TÀI LIỆU CHUYÊN ĐỀ: HỆ THỐNG TỪ LOẠI & DẤU HIỆU NHẬN BIẾT',
-        description: 'Các loại từ trong tiếng Việt .',
-        
+        description: 'Các loại từ trong tiếng Việt và mẹo xử lý.',
+        date: '12/09/2026'
     },
     {
         id: 'tailieu2',
-        title: 'Ứng dụng vòng tròn lượng giác',
-        description: 'Kỹ thuật chiếu vectơ trên sơ đồ hình tròn trong dao động.',
-        
+        title: 'TÀI LIỆU CHUYÊN ĐỀ: TÌM TỪ, THÀNH NGỮ, TỤC NGỮ KHÁC LOẠI',
+        description: 'Tuyệt chiêu phân biệt bản chất, cấu tạo và sắc thái nghĩa thay vì dịch nghĩa cảm tính.',
+        date: '15/09/2026'
     }
 ];
 
