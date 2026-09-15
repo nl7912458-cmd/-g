a9c0449ecef200202336abaca69f15a9ddb1b163
nl7912458-cmd <nl7912458@gmail.com>
2026-09-15 15:58:39 +0700

    Update list-data.js

--- data/list-data.js
+++ data/list-data.js
@@ -97,8 +97,8 @@ export const listTaiLieu = [
 export const listDeThi = [
     { id: 'de1', title: 'Đề thực chiến số 1', time: 60, questions: 50 },
     // Thêm đề thi sau này ở đây
-    { id: 'de2', title: 'Đề thực chiến số 1', time: 60, questions: 50 },
+    { id: 'de2', title: 'Đề thực chiến số 2', time: 60, questions: 50 },
     // Thêm đề thi sau này ở đây
-    { id: 'de3', title: 'Đề thực chiến số 1', time: 60, questions: 50 },
+    { id: 'de3', title: 'Đề thực chiến số 3', time: 60, questions: 50 },
     // Thêm đề thi sau này ở đây
 ];
